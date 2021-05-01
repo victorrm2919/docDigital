4caaea959663741d00cf66ef8895d24f199a27ef
Victor Ruiz <victorruiz524@gmail.com>
2021-05-01 11:39:49 -0500

    Ajustes

--- dashboard/templates/header.php
+++ dashboard/templates/header.php
@@ -11,8 +11,10 @@
     <meta name="description" content="">
     <link rel="manifest" href="../site.webmanifest">
     <link rel="shortcut icon" href="../icon.png">
-    <!--     Fonts and icons     -->
+    <!--     Fonts     -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Karla:wght@400;500;600;700;800&display=swap" rel="stylesheet">     
     <!-- FontAwesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
     <!-- CSS Files -->
@@ -20,7 +22,7 @@
     <link href="assets/css/light-bootstrap-dashboard.css?v=2.0.1" rel="stylesheet" />
     <link rel="stylesheet" href="assets/css/styles.css">
     <!-- CSS Just for demo purpose, don't include it in your project -->
-    <link href="assets/css/demo.css" rel="stylesheet" />
+    <!-- <link href="assets/css/demo.css" rel="stylesheet" /> -->
 
 </head>
 
