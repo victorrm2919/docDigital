6c13bdf004bd4d18868092457cc6e5d55da66d04
Victor Ruiz <victorruiz524@gmail.com>
2021-05-09 18:20:21 -0500

    base nueva

--- docdigital.sql
+++ docdigital.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 24, 2021 at 04:30 PM
+-- Generation Time: May 09, 2021 at 11:18 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.4.1
 
@@ -33,30 +33,19 @@ CREATE TABLE `doctor` (
   `nombres` varchar(50) NOT NULL,
   `apellidos` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(60) NOT NULL
+  `password` varchar(60) NOT NULL,
+  `fechaCreate` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `doctor`
 --
 
-INSERT INTO `doctor` (`id`, `nombres`, `apellidos`, `email`, `password`) VALUES
-(50, 'Victor', 'Ruiz', 'victorruiz@gmail.com', '$2y$10$oUj2a2XVaZyRL1kcB52kTOPYJjotHANEhhMq9IolQnZdQZltiaAnK'),
-(51, 'Victor', 'Ruiz', 'victorru@gmail.com', '$2y$10$NFBoxPwPp4nMe5ytf5pm.uiwmpbjhL1vDavU8sIW3hOZFfM0jZT86'),
-(52, 'Vick', 'Ru', 'vr@gmail.com', '$2y$10$ovUck.cX/XdfyIzK8GrQG.58Ja5vAiJQflQ5HXcqXnLMS0KC7r0lu'),
-(53, 'juan', 'peres', 'asd@sad.asd', '$2y$10$v9rwgTOQu7oV0K4MQYJnqeTV2WPPBJeRUvNFvRpq0PCronBU8ijBS'),
-(54, 'Juan', 'Perez', 'email@endjb.com', '$2y$10$sDKqPyfDXoAwrLm0V85ua.w6MefelpQv0F5DJiq./prGzAq5On6hS'),
-(55, 'Victor', 'Ruiz', 'victor@email.com', '$2y$10$NrhB41sfX31g6suuFet.tOtz9kTFidYb.9V7NXFIbRrzQ6IScZtLu'),
-(56, 'Victor', 'Ruiz M', 'victorr@gmail.com', '$2y$10$3bjrUACEMyqyMryBFHsPQ.gAtagHyi0DTEokjFWrUNVL/8zh1IDW2'),
-(57, 'v', 'v', 'c@gmail.com', '$2y$10$JOuxGtUTlqQqwy9sZM.SOe./Ar8DrX6zp7WfkqBug6Lcwnss3G5yK'),
-(58, 'victor', 'v', 'Vic@email.com', '$2y$10$mcPyvRD3meF/KkP43HArge8brmiQnmmw4H6Sg645OfuwlTKEqo/Lm'),
-(59, 'victor', 'ruiz', 'victor@gmail.com', '$2y$10$ocWgappsVHmaKZqP8kP.je0QIbiwIPlJZGnjc0b1t0DfXKEgdNpf6'),
-(60, 'victor', 'ruiz', 'v@wenje.com', '$2y$10$BsQXMTih54jXxv5goJSw1.LoH1PcWE6BXNrpLv18V2NXCTg4nX99i'),
-(61, 'askjfdb', 'sjhfdasehj', 'asd@asdwhedvhjg.com', '$2y$10$1gEslw6EA0w6EtecutVzMuySe/2QQr5B9EhPe4y/DJpClbO4w4HaW'),
-(62, 'asdasdawe', 'adwqeqwe', 'qweq@qweqwe.com', '$2y$10$lcqk20Lqzmvq1PVQwzCkduiU.tpCkaqlVgE5IKGA0IrzDjfy36WjO'),
-(63, 'Victor', 'Ruiz', 'victorR@email.com', '$2y$10$RpGHOAxAbiIRjGy9eTgEMuE5Rf0qyNSEo9KxO9nc4RBu5C2INk.4G'),
-(64, 'perez', 'juan', 'p@email.com', '$2y$10$XTIAuvcoixRd7.jrru/M5eGIATz7zVrBOtt2Ta4m0Blpy845Z8UEe'),
-(65, 'ajhsdhasjvdhjvasg', 'hjagsdhjasdhj', 'sjkkfb@asnbdvasb.com', '$2y$10$bIVnWa4N48ysD6zKRNv9xepsl6ChJAfvGRncGI4pzLJOgekHs6Saq');
+INSERT INTO `doctor` (`id`, `nombres`, `apellidos`, `email`, `password`, `fechaCreate`) VALUES
+(2, 'Victor', 'Ruiz', 'victor@email.com', '$2y$10$EEp7gh0v7IDMvPoyNe.sNOjFTW9ehxB9TuGKgM2sqs8PrjepWXAvK', '2021-05-08'),
+(3, 'Juan', 'Perez', 'j@email.com', '$2y$10$gPd.tWT0/6NYZiM49T3hpuJqQzpPDqriaYjRahraMz.uy7cOj..fW', '2021-05-09'),
+(4, 'asdasd', 'asdasd', 'asd@asd.asd', '$2y$10$AmWJDrVVBzRmkUXhJz963e9HVRW/pdrFiIcutEtQiV5qKtuzBjiW.', '2021-05-09'),
+(5, 'Victor', 'Morales', 'v@email.com', '$2y$10$lH.caT/P9IcWQHRrzPmIReu8DBQNhzlfxfEotQSlFF.nTULGEofQS', '2021-05-09');
 
 -- --------------------------------------------------------
 
@@ -65,52 +54,51 @@ INSERT INTO `doctor` (`id`, `nombres`, `apellidos`, `email`, `password`) VALUES
 --
 
 CREATE TABLE `especialidades` (
-  `Id_esp` int(11) NOT NULL,
-  `nombre` varchar(40) NOT NULL,
-  `clave` varchar(6) NOT NULL
+  `nombre` varchar(40) DEFAULT NULL,
+  `clave` varchar(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `especialidades`
 --
 
-INSERT INTO `especialidades` (`Id_esp`, `nombre`, `clave`) VALUES
-(1, 'Anestesiología ', 'ANEST'),
-(2, 'Anatomía Patológica', 'ANAPAT'),
-(3, 'Cardiología', 'CARDI'),
-(4, 'Cardiología Intervencionista', 'CARDIN'),
-(5, 'Cirugía Pediátrica ', 'CIRPED'),
-(6, 'Cirugía General', 'CIRGRL'),
-(7, 'Cirugía Plástica y Reconstructiva', 'CIPLRE'),
-(8, 'Angiología y Cirugía Vascular', 'ANCIRV'),
-(9, 'Dermatología', 'DERMA'),
-(10, 'Endoscopia', 'ENDOS'),
-(11, 'Gastroenterología', 'GASTRO'),
-(12, 'Ginegología y Obstetricia', 'GINECO'),
-(13, 'Hematología', 'HEMAT'),
-(14, 'Infectología', 'INFECT'),
-(15, 'Medicina Aeroespacial', 'MEDAER'),
-(16, 'Medicina de Rehabilitación', 'MEDREH'),
-(17, 'Medicina Interna', 'MEDINT'),
-(18, 'Nefrología', 'NEFRO'),
-(19, 'Neurología', 'NEURO'),
-(20, 'Neumología', 'NEUMO'),
-(21, 'Oftalmología', 'OFTAL'),
-(22, 'Ortopedia', 'ORTOP'),
-(23, 'Otorrinolaringología', 'OTORR'),
-(24, 'Patología Clínica ', 'PATCLI'),
-(25, 'Pediatría', 'PEDIA'),
-(26, 'Psiquiatría General', 'PSIGRL'),
-(27, 'Radiología e Imagen', 'RADIMG'),
-(28, 'Medicina del Enfermo en Estado Crítico ', 'MEDCRT'),
-(29, 'Urología', 'UROL'),
-(30, 'Cardiología Intervencionista', 'CAINTE'),
-(31, 'Neumología', 'NEUMOL'),
-(32, 'Hematología', 'HEMAT'),
-(33, 'Cirugía Oncológica', 'CIRONC'),
-(34, 'Oncología Médica', 'ONCMED'),
-(35, 'Oncología Pediátrica', 'ONCPED'),
-(36, 'Radio-Oncología', 'RADONC');
+INSERT INTO `especialidades` (`nombre`, `clave`) VALUES
+('Anatomía Patológica', 'ANAPAT'),
+('Angiología y Cirugía Vascular', 'ANCIRV'),
+('Anestesiología ', 'ANEST'),
+('Cardiología Intervencionista', 'CAINTE'),
+('Cardiología', 'CARDI'),
+('Cardiología Intervencionista', 'CARDIN'),
+('Cirugía Plástica y Reconstructiva', 'CIPLRE'),
+('Cirugía General', 'CIRGRL'),
+('Cirugía Oncológica', 'CIRONC'),
+('Cirugía Pediátrica ', 'CIRPED'),
+('Dermatología', 'DERMA'),
+('Endoscopia', 'ENDOS'),
+('Gastroenterología', 'GASTRO'),
+('Ginegología y Obstetricia', 'GINECO'),
+('Hematología', 'HEMAT'),
+('Infectología', 'INFECT'),
+('Medicina Aeroespacial', 'MEDAER'),
+('Medicina del Enfermo en Estado Crítico ', 'MEDCRT'),
+('Medicina Interna', 'MEDINT'),
+('Medicina de Rehabilitación', 'MEDREH'),
+('Nefrología', 'NEFRO'),
+('Neumología', 'NEUMO'),
+('Neumología', 'NEUMOL'),
+('Neurología', 'NEURO'),
+('Oftalmología', 'OFTAL'),
+('Oncología Médica', 'ONCMED'),
+('Oncología Pediátrica', 'ONCPED'),
+('Ortopedia', 'ORTOP'),
+('Otorrinolaringología', 'OTORR'),
+('Patología Clínica ', 'PATCLI'),
+('Pediatría', 'PEDIA'),
+('Psiquiatría General', 'PSIGRL'),
+('Radiología e Imagen', 'RADIMG'),
+('Radio-Oncología', 'RADONC'),
+('Urología', 'UROL'),
+(NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -135,8 +123,21 @@ CREATE TABLE `expedientes_doc` (
   `cedProf` double NOT NULL,
   `esp` varchar(50) DEFAULT NULL,
   `cedEsp` double DEFAULT NULL,
-  `url_firma` varchar(20) NOT NULL
+  `costoCons` int(3) NOT NULL,
+  `clabe` double NOT NULL,
+  `url_firma` varchar(20) NOT NULL,
+  `id_doc` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `expedientes_doc`
+--
+
+INSERT INTO `expedientes_doc` (`id_exp_doc`, `curp`, `rfc`, `hc`, `telPer`, `direc`, `cp`, `col`, `edo`, `mun`, `telCons`, `uni`, `titulo`, `cedProf`, `esp`, `cedEsp`, `costoCons`, `clabe`, `url_firma`, `id_doc`) VALUES
+(56, 'RUMV951029HMCZRC03', 'RUMV951029', 'VA7', 5559874876, 'Cjon Purepechas 5', 56607, 'CULTURAS DE MÉXICO', 'MÉXICO', 'CHALCO', 5559874876, 'UTEL Universidad', 'Sistemas', 12216548, '', 0, 50, 2.3657987544654656e17, 'VicRui-2.jpg', 2),
+(57, 'SDHFGHJSDGFJHSDFGH', 'SDHFGHJSDG', '156', 6565656565, 'asdjahjsdvj', 56600, 'CAMPESTRE SANTA CRUZ', 'MÉXICO', 'CHALCO', 3556465465, 'hasgdyagsdhjgfashd', 'askjdgasgfd', 26545465, 'ANEST', 23132465, 55, 5.646549878978642e17, 'JuaPer-3.jpg', 3),
+(58, 'SDASDJHAGSDAHDJGAH', 'SDASDJHAGS', '321', 6545454748, 'asjdvhgas', 56000, 'TEZOYUCA', 'MÉXICO', 'TEZOYUCA', 5465456454, 'jashdkhaasdhjk', 'hasjkdgjasdgjhasgd', 23165487, '', 0, 55, 6.514658978976547e17, 'asdasd-4.jpg', 4),
+(59, 'HSDFHJSGDFJHGGSDFH', 'HSDFHJSGDF', '546', 6556446546, 'ajksdhahdj', 56007, 'SAN FELIPE', 'MÉXICO', 'TEZOYUCA', 5465454564, 'ahsgdhjagsdhjg', 'hjkasdghjasgd', 56456489, '', 0, 50, 5.3126549878979686e17, 'VicMor-5.jpg', 5);
 
 -- --------------------------------------------------------
 
@@ -167,14 +168,6 @@ CREATE TABLE `expedientes_pac` (
   `id_pac` int(9) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `expedientes_pac`
---
-
-INSERT INTO `expedientes_pac` (`id_exp`, `sexo`, `estadoCivil`, `fechaNac`, `edad`, `edoNac`, `munNac`, `dir`, `cp`, `col`, `edo`, `mun`, `telP`, `eduPro`, `ocu`, `prof`, `relig`, `refPer`, `telRefP`, `id_pac`) VALUES
-(2, '', '', '1995-10-29', 25, 'CHI', 'ASCENSIÓN', 'asd54as1d45', 56607, 'CULTURAS DE MÉXICO', 'MÉXICO', 'ASCENSIÓN', 2411646544, 'sec', 'desemp', 'sdfsdfsdfsdf', 'sdfsdfsdf', 'sdfsdfsdf', 5618456484, 12),
-(3, 'M', 'cas', '2020-04-23', 1, 'CHI', 'ALLENDE', 'asdas 5654asd', 56607, 'CULTURAS DE MÉXICO', 'MÉXICO', 'ALLENDE', 5665465454, 'sec', 'desemp', 'ahjdjasghdh', 'asdasd', 'tgdhgasfdhgasd', 4154565456, 6);
-
 -- --------------------------------------------------------
 
 --
@@ -189,18 +182,29 @@ CREATE TABLE `paciente` (
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `paciente`
+-- Table structure for table `perfiles`
 --
 
-INSERT INTO `paciente` (`id`, `nombres`, `apellidos`, `email`, `password`) VALUES
-(6, 'v', 'v', 'v@email.com', '$2y$10$wcyfdIPs.f48BG8JN2wX4uh46t7R5tPUAK/mRCwnL0yFHiXw.X0Zi'),
-(7, 'Vcitor', 'Ap', 'f@gmail.com', '$2y$10$OEYQFyOqOWTmhvXqvoW7/ewhhqxWG0h8RyE0z4bU0RMJ/PcLTSgcC'),
-(8, 'victor', 'ruiz', 'g@gamil.com', '$2y$10$n/su8fXdGrfDACtcKuxqGeNlBM0gT5fZcf6N9NjR1HHfVDEn.I/tC'),
-(9, 'victor', 'ruiz', 'victor@gmail.com', '$2y$10$B.P5MslTi6QOUjh2osbgtetTp76eBv5OecG.vT9LCjFsWrEh3C63W'),
-(10, 'victor', 'victor', 'v@gmail.com', '$2y$10$uz8qE27Rxash6SIa0Ha79OVHv090Qkp6pCGqrsOnt7Kih8mYLkcJS'),
-(11, 'hasd', 'hjasdhj', 'skjdb@asdjk.com', '$2y$10$nICCQ98mjQee/yzLnv5x/u1iK/BzfSn3wF0QMFeR/735DD/cdgnJS'),
-(12, 'Victor', 'Ruiz', 'victorRui@gmail.com', '$2y$10$nnzcU81Yk2GARGtH6yayEebwtnRcCv0T.2K5yFSc3eN5xGpI43t/u');
+CREATE TABLE `perfiles` (
+  `id_perfil` int(11) NOT NULL,
+  `doctor` int(11) NOT NULL,
+  `clave_esp` varchar(10) DEFAULT NULL,
+  `descr` varchar(100) DEFAULT 'Sin descripcion',
+  `foto` varchar(25) NOT NULL DEFAULT 'default-avatar.png'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `perfiles`
+--
+
+INSERT INTO `perfiles` (`id_perfil`, `doctor`, `clave_esp`, `descr`, `foto`) VALUES
+(20, 2, NULL, 'Sin descripcion', 'default-avatar.png'),
+(21, 3, 'ANEST', 'Sin descripcion', 'default-avatar.png'),
+(22, 4, '', 'Sin descripcion', 'default-avatar.png'),
+(23, 5, '', 'Sin descripcion', 'default-avatar.png');
 
 --
 -- Indexes for dumped tables
@@ -216,13 +220,14 @@ ALTER TABLE `doctor`
 -- Indexes for table `especialidades`
 --
 ALTER TABLE `especialidades`
-  ADD PRIMARY KEY (`Id_esp`);
+  ADD UNIQUE KEY `clave` (`clave`);
 
 --
 -- Indexes for table `expedientes_doc`
 --
 ALTER TABLE `expedientes_doc`
-  ADD PRIMARY KEY (`id_exp_doc`);
+  ADD PRIMARY KEY (`id_exp_doc`),
+  ADD KEY `id_doc` (`id_doc`);
 
 --
 -- Indexes for table `expedientes_pac`
@@ -238,6 +243,13 @@ ALTER TABLE `paciente`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `perfiles`
+--
+ALTER TABLE `perfiles`
+  ADD PRIMARY KEY (`id_perfil`),
+  ADD KEY `id_doctor` (`doctor`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -245,41 +257,53 @@ ALTER TABLE `paciente`
 -- AUTO_INCREMENT for table `doctor`
 --
 ALTER TABLE `doctor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
-
---
--- AUTO_INCREMENT for table `especialidades`
---
-ALTER TABLE `especialidades`
-  MODIFY `Id_esp` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `expedientes_doc`
 --
 ALTER TABLE `expedientes_doc`
-  MODIFY `id_exp_doc` int(9) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_exp_doc` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
 
 --
 -- AUTO_INCREMENT for table `expedientes_pac`
 --
 ALTER TABLE `expedientes_pac`
-  MODIFY `id_exp` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_exp` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `paciente`
 --
 ALTER TABLE `paciente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `perfiles`
+--
+ALTER TABLE `perfiles`
+  MODIFY `id_perfil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `expedientes_doc`
+--
+ALTER TABLE `expedientes_doc`
+  ADD CONSTRAINT `expedientes_doc_ibfk_1` FOREIGN KEY (`id_doc`) REFERENCES `doctor` (`id`);
+
+--
 -- Constraints for table `expedientes_pac`
 --
 ALTER TABLE `expedientes_pac`
   ADD CONSTRAINT `expedientes_pac_ibfk_1` FOREIGN KEY (`id_pac`) REFERENCES `paciente` (`id`);
+
+--
+-- Constraints for table `perfiles`
+--
+ALTER TABLE `perfiles`
+  ADD CONSTRAINT `perfiles_ibfk_1` FOREIGN KEY (`doctor`) REFERENCES `doctor` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
